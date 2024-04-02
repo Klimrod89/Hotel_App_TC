9105af76ce6d84d0c7fd59510e38613c3cbe3abc
Yassir_3630 <koutoukale+GitOnVS@gmail.com>
2024-04-01 22:30:24 -0400

    Created Database

---
+++ HotelAppDB/dbo/Tables/Bookings.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Bookings]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[RoomId] INT NOT NULL,
+	[GuestId] INT NOT NULL,
+	[StartDate] DATETIME2(7) NOT NULL,
+	[EndDate] DATETIME2(7) NOT NULL,
+	[TotalCost] Money NOT NULL,
+	[CheckIn] BIT NOT NULL Default 0,
+	Constraint [FK_Bookings_Rooms] FOREIGN KEY ([RoomId]) REFERENCES [dbo].[Rooms]([Id]),
+	Constraint [FK_Bookings_Guests] FOREIGN KEY ([GuestId]) REFERENCES [dbo].[Guests]([Id])
+)
---
+++ HotelAppDB/dbo/Tables/Rooms.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Rooms]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[RoomTypeId] INT NOT NULL,
+	[RoomNumber] VARCHAR(10) NOT NULL,
+	Constraint [FK_Rooms_RoomTypes] FOREIGN KEY ([RoomTypeId]) REFERENCES [dbo].[RoomTypes]([Id])
+)
